3f30b28caad0b8de6592dfb488ce193ae07d126f
aishwarya <aishwarya@aishwarya-Lenovo-Z580>
2014-01-02 11:48:53 +0530

    Removed Groovy

--- GroovyWorkOut3-17-8/src/aish/vaishno/grovvyworkout3/executers/Executer.groovy
+++
@@ -1,8 +0,0 @@
-package aish.vaishno.grovvyworkout3.executers
-
-FileCallerGroovy.append();
-
-FileCallerGroovy.getContent()
-	
-FileCallerGroovy fileCallerGroovy=new FileCallerGroovy();
-println '\n\nchecking out without Return: '+fileCallerGroovy.name();
